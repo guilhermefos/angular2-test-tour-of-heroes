5443185f9eb8e50aeca65f8bff668291a2b2c66e
guilhermefos <guilhermefos.developer@gmail.com>
2016-11-10 09:40:04 -0200

    complete welcome test

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -9,56 +9,81 @@ import { WelcomeComponent } from './welcome.component';
 
 describe('Component: Welcome', () => {
 
-	let comp: WelcomeComponent;
-	let fixture: ComponentFixture<WelcomeComponent>;
-	let componentUserService: UserService;
-	let de: DebugElement;
-	let el: HTMLElement;
-	let userServiceStub;
-	let userService;
+  let comp: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
+  let componentUserService: UserService; // the actually injected service
+  let userService: UserService; // the TestBed injected service
+  let de: DebugElement;  // the DebugElement with the welcome message
+  let el: HTMLElement; // the DOM element with the welcome message
 
-	beforeEach(() => {
-		// stub UserService for test purposes
-		userServiceStub = {
-			isLoggedIn: true,
-			user: { name: 'Test User'}
-		};
-	});
+  let userServiceStub: {
+    isLoggedIn: boolean;
+    user: { name: string}
+  };
 
-	TestBed.configureTestingModule({
-		declarations: [ WelcomeComponent ],
-		providers: [ {provide: UserService, useValue: userServiceStub }]
-	});
+  beforeEach(() => {
+    // stub UserService for test purposes
+    userServiceStub = {
+      isLoggedIn: true,
+      user: { name: 'Test User'}
+    };
 
-	fixture = TestBed.createComponent(WelcomeComponent);
-	comp = fixture.componentInstance;
+    TestBed.configureTestingModule({
+       declarations: [ WelcomeComponent ],
+    // providers:    [ UserService ]  // NO! Don't provide the real service!
+                                      // Provide a test-double instead
+       providers:    [ {provide: UserService, useValue: userServiceStub } ]
+    });
 
-	// UserService from the root injector
-	userService = TestBed.get(UserService);
+    fixture = TestBed.createComponent(WelcomeComponent);
+    comp    = fixture.componentInstance;
 
-	// get the "welcome" element by CSS selector (e.g., by class name)
-	de = fixture.debugElement.query(By.css('.welcome'));
-	el = de.nativeElement;
+    // UserService actually injected into the component
+    userService = fixture.debugElement.injector.get(UserService);
+    componentUserService = userService;
+    // UserService from the root injector
+    userService = TestBed.get(UserService);
 
-	it('should welcome the user', () => {
-		fixture.detectChanges();
-		const content = el.textContent;
-		expect(content).toContain('Welcome', '"Welcome ..."');
-		expect(content).toContain('Test User', 'expected name');
-	});
+    //  get the "welcome" element by CSS selector (e.g., by class name)
+    de = fixture.debugElement.query(By.css('.welcome'));
+    el = de.nativeElement;
+  });
 
-	it('should welcome "Bubba"', () => {
-		userService.user.name = 'Babba'; // welcome message hasn't been shown yet
-		fixture.detectChanges();
-		expect(el.textContent).toContain('Bubba');
-	});
+  it('should welcome the user', () => {
+    fixture.detectChanges();
+    const content = el.textContent;
+    expect(content).toContain('Welcome', '"Welcome ..."');
+    expect(content).toContain('Test User', 'expected name');
+  });
 
-	it('should request login if not logged in', () => {
-		userService.isLoggedIn = false; // welcome message hasn't been shown yet
-		fixture.detectChanges();
-		const content = el.textContent;
-		expect(content).not.toContain('Welcome', 'not welcomed');
-		expect(content).toMatch(/log in/i, '"log in"');
-	});
+  it('should welcome "Bubba"', () => {
+    userService.user.name = 'Bubba'; // welcome message hasn't been shown yet
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Bubba');
+  });
 
+  it('should request login if not logged in', () => {
+    userService.isLoggedIn = false; // welcome message hasn't been shown yet
+    fixture.detectChanges();
+    const content = el.textContent;
+    expect(content).not.toContain('Welcome', 'not welcomed');
+    expect(content).toMatch(/log in/i, '"log in"');
+  });
+
+  it('should inject the component\'s UserService instance',
+    inject([UserService], (service: UserService) => {
+    expect(service).toBe(componentUserService);
+  }));
+
+  it('TestBed and Component UserService should be the same', () => {
+    expect(userService === componentUserService).toBe(true);
+  });
+
+  it('stub object and injected UserService should not be the same', () => {
+    expect(userServiceStub === userService).toBe(false);
+
+    // Changing the stub object has no effect on the injected service
+    userServiceStub.isLoggedIn = false;
+    expect(userService.isLoggedIn).toBe(true);
+  });
 });
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+	welcome = '-- not initialized yet --';
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  	this.welcome = this.userService.isLoggedIn ?
+  	'Welcome, ' + this.userService.user.name :
+  	'Please log in.';
   }
-
 }
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
